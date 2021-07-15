e925ce9e1322e81b72b9f0cd523464fc4b1f10fc
Angelica Cortez <angelica.cortez@avanade.com>
2021-07-15 09:26:42 +0200

    Add project files.

---
+++ Week1Day4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Week1Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //alternativa if else
+            int a = 1;
+            int b = 2;
+            string c;
+            if (a < b)
+            {
+                c = " a è più piccolo di b";
+
+            }
+            else 
+            {
+                c = "a è più grande di b";
+            }
+
+            c = (a < b) ? "a è più piccolo di b" : "a è più grande di b";
+            int pari = 0;
+            int dispari = 0;
+
+            int n = 10;
+            ((n % 2 == 0) ? ref pari : ref dispari)++;
+
+           
+        }
+    }
+}
