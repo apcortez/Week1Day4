427242c8202a9016068aeb58305027ec2d20358e
Angelica Cortez <angelica.cortez@avanade.com>
2021-07-15 09:27:42 +0200

    ternario: alternativa if-else

--- Week1Day4/Program.cs
+++ Week1Day4/Program.cs
@@ -6,7 +6,7 @@ namespace Week1Day4
     {
         static void Main(string[] args)
         {
-            //alternativa if else
+            //alternativa if else - ternario
             int a = 1;
             int b = 2;
             string c;
@@ -19,7 +19,7 @@ namespace Week1Day4
             {
                 c = "a è più grande di b";
             }
-
+            // if ? (allora) : (else allora);
             c = (a < b) ? "a è più piccolo di b" : "a è più grande di b";
             int pari = 0;
             int dispari = 0;
