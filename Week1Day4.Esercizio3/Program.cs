4d693bbe8e7a09fbc484d48bdc14864228cce3f2
Angelica Cortez <angelica.cortez@avanade.com>
2021-07-15 12:37:43 +0200

    traccia es3

---
+++ Week1Day4.Esercizio3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week1Day4.Esercizio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
